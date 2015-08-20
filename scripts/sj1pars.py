6cfdb97d344c5c659c787ae08f422110d56f0d95
nadiahpk <nadiah.kristensen@gmail.com>
2015-08-20 15:06:14 +1000

    Gives ambiguous edges example

---
+++ scripts/sj1pars.py
@@ -0,0 +1,10 @@
+# Simple ambiguous edges for now, we can expand on this later
+
+ambig_edges_list = [
+    [],
+    [('mouse', 'scrubOak')],
+    [('treesBig', 'fox')],
+    [('gopherSnake', 'goldenEagle')],
+    [('gopherSnake', 'baldEagle')],
+    [('treesBig', 'manzanita')],
+]
